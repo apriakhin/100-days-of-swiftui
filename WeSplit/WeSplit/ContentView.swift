e0751dc72f16782298eaf1e307d70057bf0249be
apriakhin <apriakhin@aol.com>
2021-12-23 01:08:51 +0300

    Change the tip percentage picker to show a new screen

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -13,8 +13,6 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let amountPerPerson = totalAmount / peopleCount
@@ -47,11 +45,10 @@ struct ContentView: View {
                 
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
