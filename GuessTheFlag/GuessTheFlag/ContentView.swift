f9298f967c2a454b811bb1baa5dbf9f7f3415ec1
apriakhin <apriakhin@aol.com>
2021-12-27 21:39:46 +0300

    Add buttons

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Anton Priakhin on 27.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }
+                .foregroundColor(.white)
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        // Flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
