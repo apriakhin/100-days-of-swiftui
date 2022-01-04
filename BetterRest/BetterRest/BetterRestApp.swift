3b2555140a7f2db1242f2be6fc5f3055ca833768
apriakhin <apriakhin@aol.com>
2022-01-04 13:51:37 +0300

    Create basic layout

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anton Priakhin on 04.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
