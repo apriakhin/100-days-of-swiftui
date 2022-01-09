8dfbc19191eaf3245ab40afb130dda3bfcc6c7c1
apriakhin <apriakhin@aol.com>
2022-01-09 13:47:39 +0300

    Create implicit animations

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Anton Priakhin on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
