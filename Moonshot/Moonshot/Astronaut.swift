ae8dea47b66a6414c1e4e4e09557755c334224f1
apriakhin <apriakhin@aol.com>
2022-01-14 22:38:36 +0300

    Add load data from json

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Anton Priakhin on 14.01.2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Anton Priakhin on 14.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
+    var body: some View {
+        Text("\(astronauts.count)")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
