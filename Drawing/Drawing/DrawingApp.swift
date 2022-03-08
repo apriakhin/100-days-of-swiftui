921bdf92cb97594037317e6eacc7649384476199
apriakhin <apriakhin@aol.com>
2022-03-08 12:55:50 +0300

    Create custom paths

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Anton Priakhin on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
