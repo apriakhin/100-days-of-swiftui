d4f72cb21971e27d3f16d226d8ed122878781500
apriakhin <apriakhin@aol.com>
2022-04-12 23:06:41 +0300

    Create custom component with @Binding

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Anton Priakhin on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
