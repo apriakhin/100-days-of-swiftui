05d83bd07ce163020784f7f574f39289fd689043
apriakhin <apriakhin@aol.com>
2022-04-17 01:00:36 +0300

    Add deleting from a Core Data fetch request

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -37,21 +37,36 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
-           .navigationTitle("Bookworm")
-           .toolbar {
-               ToolbarItem(placement: .navigationBarTrailing) {
-                   Button {
-                       showingAddScreen.toggle()
-                   } label: {
-                       Label("Add Book", systemImage: "plus")
-                   }
-               }
-           }
-           .sheet(isPresented: $showingAddScreen) {
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label("Add Book", systemImage: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
                AddBookView()
-           }
-       }
+            }
+        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+
+            moc.delete(book)
+        }
+
+        try? moc.save()
     }
 }
 
