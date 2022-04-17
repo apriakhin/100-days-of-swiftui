854bf423a2ce19a76e2211678bc251d037bc6034
apriakhin <apriakhin@aol.com>
2022-04-17 17:11:19 +0300

    Create app

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Anton Priakhin on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,20 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Anton Priakhin on 17.04.2022.
+//
+
+import CoreData
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
