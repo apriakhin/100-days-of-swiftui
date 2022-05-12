c9ae6ec53bfdf0e8514bb5195dbfd956826116e0
apriakhin <apriakhin@aol.com>
2022-05-12 22:08:25 +0300

    Add conformance to Comparable for custom types

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Anton Priakhin on 21.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
